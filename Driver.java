bd8f7379218dfd6c677bd17c3f9f4d7c467a6c5d
tanzimelahi <telahi00@stuy.edu>
2019-02-11 22:44:29 -0500

    final version, I will add the step by step commits later, I don't have time tongiht for tomorrow I have a huge physics test

---
+++ Driver.java
@@ -0,0 +1,11 @@
+public class  Driver{
+  public static void main(String[]args){
+    QueenBoard test=new QueenBoard(8);
+   System.out.println(  test.solveR());
+
+   System.out.println(test);
+   QueenBoard test2=new QueenBoard(3);
+   test2.solveR();
+   System.out.println(test2);
+  }
+}
